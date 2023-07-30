564972577219a838b67d4a8ceb5d3f167d11fe36
Jarvis <18375166@buaa.edu.cn>
2023-07-30 22:23:03 +0800

    course code

--- run.py
+++ run.py
@@ -1,30 +1,31 @@
 import numpy as np
-import sys
 import gc
-import gzip
-import pickle
-#import openml
-import os
 import argparse
 from dataset import get_dataset, get_handler
-from model import get_net
-import vgg
-import resnet
 from sklearn.preprocessing import LabelEncoder
 import torch.nn.functional as F
 from torch import nn
 from torchvision import transforms
 import torch
-import time
-import pdb
-from scipy.stats import zscore
+import pandas as pd
+
+class logger():
+    def __init__(self, path):
+        self.path = path
+        self.result = ""
+
+    def log(self, result):
+        self.result += result +"\n"
+
+    def save_result(self):
+        with open(self.path, "w") as file:
+            file.write(self.result)
+        file.close()
+
 
 from query_strategies import RandomSampling, BadgeSampling, \
-                                BaselineSampling, LeastConfidence, MarginSampling, \
-                                EntropySampling, CoreSet, ActiveLearningByLearning, \
-                                LeastConfidenceDropout, MarginSamplingDropout, EntropySamplingDropout, \
-                                KMeansSampling, KCenterGreedy, BALDDropout, CoreSet, \
-                                AdversarialBIM, AdversarialDeepFool, ActiveLearningByLearning, BaitSampling
+                                LeastConfidence, MarginSampling, \
+                                EntropySampling,  CoreSet
 
 
 parser = argparse.ArgumentParser()
@@ -34,9 +35,9 @@ parser.add_argument('--lr', help='learning rate', type=float, default=1e-4)
 parser.add_argument('--model', help='model - resnet, vgg, or mlp', type=str, default='mlp')
 parser.add_argument('--path', help='data path', type=str, default='data')
 parser.add_argument('--data', help='dataset (non-openML)', type=str, default='')
-parser.add_argument('--nQuery', help='number of points to query in a batch', type=int, default=100)
-parser.add_argument('--nStart', help='number of points to start', type=int, default=100)
-parser.add_argument('--nEnd', help = 'total number of points to query', type=int, default=50000)
+parser.add_argument('--nQuery', help='number of points to query in a batch', type=int, default=200)
+parser.add_argument('--nStart', help='number of points to start', type=int, default=1000)
+parser.add_argument('--nEnd', help = 'total number of points to query', type=int, default=11636)
 parser.add_argument('--nEmb', help='number of embedding dims (mlp)', type=int, default=128)
 parser.add_argument('--rounds', help='number of rounds (0 does entire dataset)', type=int, default=0)
 parser.add_argument('--trunc', help='dataset truncation (-1 is no truncation)', type=int, default=-1)
@@ -51,112 +52,53 @@ NUM_QUERY = opts.nQuery
 NUM_ROUND = int((opts.nEnd - NUM_INIT_LB)/ opts.nQuery)
 DATA_NAME = opts.data
 
-# regularization settings for bait
-opts.lamb = 1
-if 'CIFAR' in opts.data: opts.lamb = 1e-2
-
-# non-openml data defaults
-args_pool = {'MNIST':
-                {'n_epoch': 10, 'transform': transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]),
-                 'loader_tr_args':{'batch_size': 64, 'num_workers': 0},
-                 'loader_te_args':{'batch_size': 1000, 'num_workers': 0},
-                 'optimizer_args':{'lr': 0.01, 'momentum': 0.5}},
-            'FashionMNIST':
-                {'n_epoch': 10, 'transform': transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]),
-                 'loader_tr_args':{'batch_size': 64, 'num_workers': 1},
-                 'loader_te_args':{'batch_size': 1000, 'num_workers': 1},
-                 'optimizer_args':{'lr': 0.01, 'momentum': 0.5}},
-            'SVHN':
-                {'n_epoch': 20, 'transform': transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.4377, 0.4438, 0.4728), (0.1980, 0.2010, 0.1970))]),
-                 'loader_tr_args':{'batch_size': 64, 'num_workers': 1},
-                 'loader_te_args':{'batch_size': 1000, 'num_workers': 1},
-                 'optimizer_args':{'lr': 0.01, 'momentum': 0.5}},
-            'CIFAR10':
-                {'n_epoch': 3, 'transform': transforms.Compose([ 
-                     transforms.RandomCrop(32, padding=4),
-                     transforms.RandomHorizontalFlip(),
-                     transforms.ToTensor(),
-                     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616))
-                 ]),
-                 'loader_tr_args':{'batch_size': 128, 'num_workers': 1},
-                 'loader_te_args':{'batch_size': 1000, 'num_workers': 1},
-                 'optimizer_args':{'lr': 0.05, 'momentum': 0.3},
-                 'transformTest': transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616))])},
-                'Beijing': {
-                    'n_epoch':10,
-                    'loader_tr_args': {"batch_size": 64, 'num_workers':0},
-                     'loader_te_args':{'batch_size': 1000, 'num_workers': 0},
-                     'optimizer_args':{'lr': 0.01, 'momentum': 0.5},
-                }
-                }
-opts.nClasses = 10
-if opts.aug == 0: 
-    args_pool['CIFAR10']['transform'] =  args_pool['CIFAR10']['transformTest'] # remove data augmentation
-args_pool['MNIST']['transformTest'] = args_pool['MNIST']['transform']
-args_pool['FashionMNIST']['transformTest'] = args_pool['FashionMNIST']['transform']
-args_pool['SVHN']['transformTest'] = args_pool['SVHN']['transform']
-
-if opts.did == 0:
-    args = args_pool[DATA_NAME]
-if not os.path.exists(opts.path):
-    os.makedirs(opts.path)
 
 
-# load openml dataset if did is supplied
-if opts.did > 0:
-    data = pickle.load(open('oml/data_' + str(opts.did) + '.pk', 'rb'))['data']
-    X = np.asarray(data[0])
-    y = np.asarray(data[1])
-    y = LabelEncoder().fit(y).transform(y)
-    opts.nClasses = int(max(y) + 1)
-    nSamps, opts.dim = np.shape(X)
-    testSplit = .1
-    inds = np.random.permutation(nSamps)
-    X = X[inds]
-    y = y[inds]
+data_path = "./data/bj_dataset/poi_func_label_640000.npy"
+embedding_path = "./data/bj_dataset/embedding_HIER_bj_dataset_128.npy"
+embed = np.load(embedding_path)
+data = np.load(data_path)
+X = embed
+y = data[:,1]
+y = LabelEncoder().fit(y).transform(y)
+opts.nClasses = int(max(y) + 1)
+nSamps, opts.dim = np.shape(X)
+testSplit = 0.1
+inds = np.random.permutation(nSamps)
+X = X[inds]
+y = y[inds]
 
 
-    split =int((1. - testSplit) * nSamps)
-    while True:
-        inds = np.random.permutation(split)
-        if len(inds) > 50000: inds = inds[:50000]
-        X_tr = X[:split]
-        X_tr = X_tr[inds]
-        X_tr = torch.Tensor(X_tr)
+split =int((1. - testSplit) * nSamps)
+while True:
+    inds = np.random.permutation(split)
+    if len(inds) > 50000:
+        inds = inds[:50000]
+    X_tr = X[:split]
+    X_tr = X_tr[inds]
+    X_tr = torch.Tensor(X_tr)
 
-        y_tr = y[:split]
-        y_tr = y_tr[inds]
-        Y_tr = torch.Tensor(y_tr).long()
+    y_tr = y[:split]
+    y_tr = y_tr[inds]
+    Y_tr = torch.Tensor(y_tr).long()
 
-        X_te = torch.Tensor(X[split:])
-        Y_te = torch.Tensor(y[split:]).long()
+    X_te = torch.Tensor(X[split:])
+    Y_te = torch.Tensor(y[split:]).long()
 
-        if len(np.unique(Y_tr)) == opts.nClasses: break
-
-
-    args = {'transform':transforms.Compose([transforms.ToTensor()]),
-            'n_epoch':10,
-            'loader_tr_args':{'batch_size': 128, 'num_workers': 1},
-            'loader_te_args':{'batch_size': 1000, 'num_workers': 1},
-            'optimizer_args':{'lr': 0.01, 'momentum': 0},
-            'transformTest':transforms.Compose([transforms.ToTensor()])}
-    handler = get_handler('other')
-
-# load non-openml dataset
-else:
-    X_tr, Y_tr, X_te, Y_te = get_dataset(DATA_NAME, opts.path)
-    opts.dim = np.shape(X_tr)[1:]
-    handler = get_handler(opts.data)
+    if len(np.unique(Y_tr)) == opts.nClasses:
+        break
 
 
-if opts.trunc != -1:
-   inds = np.random.permutation(len(X_tr))[:opts.trunc]
-   X_tr = X_tr[inds]
-   Y_tr = Y_tr[inds]
-   inds = torch.where(Y_tr < 10)[0]
-   X_tr = X_tr[inds]
-   Y_tr = Y_tr[inds]
-   opts.nClasses = int(max(Y_tr) + 1)
+args = {'transform':transforms.Compose([transforms.ToTensor()]),
+        'n_epoch':10,
+        'loader_tr_args':{'batch_size': 128, 'num_workers': 0},
+        'loader_te_args':{'batch_size': 1000, 'num_workers': 0},
+        'optimizer_args':{'lr': 0.01, 'momentum': 0},
+        'transformTest':transforms.Compose([transforms.ToTensor()])}
+handler = get_handler('other')
+
+
+
 
 args['lr'] = opts.lr
 args['modelType'] = opts.model
@@ -175,18 +117,6 @@ idxs_tmp = np.arange(n_pool)
 np.random.shuffle(idxs_tmp)
 idxs_lb[idxs_tmp[:NUM_INIT_LB]] = True
 
-# linear model class
-class linMod(nn.Module):
-    def __init__(self, dim=28):
-        super(linMod, self).__init__()
-        self.dim = dim
-        self.lm = nn.Linear(dim, opts.nClasses)
-    def forward(self, x):
-        x = x.view(-1, self.dim)
-        out = self.lm(x)
-        return out, x
-    def get_embedding_dim(self):
-        return self.dim
 
 # mlp model class
 class mlpMod(nn.Module):
@@ -207,31 +137,18 @@ class mlpMod(nn.Module):
         return self.embSize
 
 # load specified network
-if opts.model == 'mlp':
-    net = mlpMod(opts.dim, embSize=opts.nEmb)
-elif opts.model == 'resnet':
-    net = resnet.ResNet18()
-elif opts.model == 'vgg':
-    net = vgg.VGG('VGG16')
-elif opts.model == 'lin':
-    dim = np.prod(list(X_tr.shape[1:]))
-    net = linMod(dim=dim)
-else: 
-    print('choose a valid model - mlp, resnet, or vgg', flush=True)
-    raise ValueError
 
-if opts.did > 0 and opts.model != 'mlp':
-    print('openML datasets only work with mlp', flush=True)
-    raise ValueError
+net = mlpMod(opts.dim, embSize=opts.nEmb)
+
 
 if type(X_tr[0]) is not np.ndarray:
     X_tr = X_tr.numpy()
 
-# set up the specified sampler
+
+
+if type(X_te) == torch.Tensor: X_te = X_te.numpy()
 if opts.alg == 'rand': # random sampling
     strategy = RandomSampling(X_tr, Y_tr, idxs_lb, net, handler, args)
-elif opts.alg == 'bait': # bait sampling
-    strategy = BaitSampling(X_tr, Y_tr, idxs_lb, net, handler, args)
 elif opts.alg == 'conf': # confidence-based sampling
     strategy = LeastConfidence(X_tr, Y_tr, idxs_lb, net, handler, args)
 elif opts.alg == 'marg': # margin-based sampling
@@ -242,29 +159,38 @@ elif opts.alg == 'coreset': # coreset sampling
     strategy = CoreSet(X_tr, Y_tr, idxs_lb, net, handler, args)
 elif opts.alg == 'entropy': # entropy-based sampling
     strategy = EntropySampling(X_tr, Y_tr, idxs_lb, net, handler, args)
-elif opts.alg == 'baseline': # badge but with k-DPP sampling instead of k-means++
-    strategy = BaselineSampling(X_tr, Y_tr, idxs_lb, net, handler, args)
-elif opts.alg == 'albl': # active learning by learning
-    albl_list = [LeastConfidence(X_tr, Y_tr, idxs_lb, net, handler, args),
-        CoreSet(X_tr, Y_tr, idxs_lb, net, handler, args)]
-    strategy = ActiveLearningByLearning(X_tr, Y_tr, idxs_lb, net, handler, args, strategy_list=albl_list, delta=0.1)
-else: 
+else:
     print('choose a valid acquisition function', flush=True)
     raise ValueError
-
-# print info
-if opts.did > 0: DATA_NAME='OML' + str(opts.did)
 print(DATA_NAME, flush=True)
 print(type(strategy).__name__, flush=True)
 
-if type(X_te) == torch.Tensor: X_te = X_te.numpy()
+Logger = logger("./result/"+opts.alg+'.txt')
 
 # round 0 accuracy
-strategy.train()
-P = strategy.predict(X_te, Y_te)
+strategy.train(verbose=False)
+P, Preds = strategy.predict(X_te, Y_te)
 acc = np.zeros(NUM_ROUND+1)
 acc[0] = 1.0 * (Y_te == P).sum().item() / len(Y_te)
+
+def top_n_accuracy(truths, preds, n):
+    best_n = np.argsort(preds, axis=1)[:, -n:]
+    successes = 0
+    for i, truth in enumerate(truths):
+        if truth in best_n[i, :]:
+            successes += 1
+    return float(successes) / truths.shape[0]
+
+
+def cal_metric(pre_dists, labels, top_n_list=[1,2,3,4,5]):
+    top_n_acc = [top_n_accuracy(labels, pre_dists, n) for n in top_n_list]
+    score_series = pd.Series(top_n_acc,
+                             index=['acc@{}'.format(n) for n in top_n_list])
+    return score_series
+res = cal_metric(Preds, Y_te)
+Logger.log('Round 0: \ttesting acc@1 {}, acc@5 {}'.format(res['acc@1'], res['acc@5']))
 print(str(opts.nStart) + '\ttesting accuracy {}'.format(acc[0]), flush=True)
+
 
 for rd in range(1, NUM_ROUND+1):
     print('Round {}'.format(rd), flush=True)
@@ -279,13 +205,16 @@ for rd in range(1, NUM_ROUND+1):
     idxs_lb[q_idxs] = True
 
     # update
-    strategy.update(idxs_lb)
+    strategy.update(idxs_lb )
     strategy.train(verbose=False)
 
     # round accuracy
-    P = strategy.predict(X_te, Y_te)
+    P, Preds = strategy.predict(X_te, Y_te)
     acc[rd] = 1.0 * (Y_te == P).sum().item() / len(Y_te)
+    res = cal_metric(Preds,Y_te)
+    Logger.log('Round {}: \ttesting acc@1 {}, acc@5 {}'.format(rd, res['acc@1'], res['acc@5']))
     print(str(sum(idxs_lb)) + '\t' + 'testing accuracy {}'.format(acc[rd]), flush=True)
     if sum(~strategy.idxs_lb) < opts.nQuery: break
     if opts.rounds > 0 and rd == (opts.rounds - 1): break
+    Logger.save_result()
 
